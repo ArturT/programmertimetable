fca12cf958c1a5f4e16c9bef9cbb1f51cbac1186
Phillip Boksz <phillip.boksz@llp.pl>
2012-07-02 15:35:37 +0200

    Added ability to add programmers when creating new project.

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,4 @@
 class Project < ActiveRecord::Base
    has_and_belongs_to_many :programmers
-   attr_accessible :end_date, :name, :start_date
+   attr_accessible :end_date, :name, :start_date, :programmer_ids
 end
